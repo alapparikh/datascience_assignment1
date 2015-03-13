3aec516fb64f44cdf333eb27376742c7bced7e82
alapparikh <alap18@gmail.com>
2015-03-13 02:00:59 -0400

    data file committed

--- data_prep.py
+++ data_prep.py
@@ -8,6 +8,24 @@ files = ['googlebooks-eng-all-1gram-20120701-v', 'googlebooks-eng-all-1gram-2012
 #Words to be used as features
 feature_words = ['volcano', 'volcanos', 'eruption', 'lava', 'molten', 'igneous', 'volcanic activity', 'volcanic eruption']
 
+count_dict = {}
+
+def get_total_count():
+	count = 0
+	with open('googlebooks-eng-all-totalcounts-20120701.txt', 'rU') as fp:
+
+		for line in fp:
+			rows = line.split('\t')
+
+		for row in rows:
+			count += 1
+			values = row.split(',')
+			
+			if int(values[0]) > 1749:
+				count_dict[values[0]] = int(values[1])
+
+	fp.close()
+
 #Get relevant data from each ngram file
 def parse_data():
 
@@ -29,11 +47,17 @@ def parse_data():
 
 		write_to_csv (relevant_data)
 
+		fp.close()
+
 def write_to_csv(data):
 	with open('word_data.csv', 'a') as fp:
 		a = csv.writer(fp, delimiter='\t')
 		for row in data:
-			a.writerow([row[0], row[1], row[2]])
+			normalized_count = float(row[2])/count_dict[row[1]]
+			a.writerow([row[0], row[1], normalized_count])
+
+	fp.close()
 
 if __name__ == '__main__':
+	get_total_count()
 	parse_data()
