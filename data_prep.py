59aa0c1850ea0016ea140a1254576a16839d9f29
alapparikh <alap18@gmail.com>
2015-03-13 02:49:40 -0400

    added comments

--- data_prep.py
+++ data_prep.py
@@ -8,20 +8,24 @@ files = ['googlebooks-eng-all-1gram-20120701-v', 'googlebooks-eng-all-1gram-2012
 #Words to be used as features
 feature_words = ['volcano', 'volcanos', 'eruption', 'lava', 'molten', 'igneous', 'volcanic activity', 'volcanic eruption']
 
+#Dictionary containing total count of words in each year
 count_dict = {}
 
+#Only import words after this year
+begin_year = 1750
+
+#Populate count dictionary
 def get_total_count():
-	count = 0
+	
 	with open('googlebooks-eng-all-totalcounts-20120701.txt', 'rU') as fp:
 
 		for line in fp:
 			rows = line.split('\t')
 
 		for row in rows:
-			count += 1
 			values = row.split(',')
 			
-			if int(values[0]) > 1749:
+			if int(values[0]) > begin_year:
 				count_dict[values[0]] = int(values[1])
 
 	fp.close()
@@ -40,7 +44,7 @@ def parse_data():
 				count += 1;
 
 				if values[0] in feature_words:
-					if int(values[1]) > 1750:
+					if int(values[1]) > begin_year:
 						relevant_data.append(values)
 
 			print len(relevant_data)
@@ -49,10 +53,14 @@ def parse_data():
 
 		fp.close()
 
+#Write relevant data to CSV file
 def write_to_csv(data):
 	with open('word_data.csv', 'a') as fp:
+
 		a = csv.writer(fp, delimiter='\t')
+
 		for row in data:
+			#Divide specific word count by total word count for that year to normalize it
 			normalized_count = float(row[2])/count_dict[row[1]]
 			a.writerow([row[0], row[1], normalized_count])
 
