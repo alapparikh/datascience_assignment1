0bc26d08bb65a7260476c03e3fc4b443bb710824
shawnbr <shawn.bramson@gmail.com>
2015-03-15 15:27:42 -0400

    added shifting, additional classifiers, added permutations spray n' pray style

--- analysis.py
+++ analysis.py
@@ -4,6 +4,13 @@ import numpy as np
 import data_prep as dp
 from sklearn import preprocessing
 from sklearn import linear_model
+from sklearn import preprocessing
+from sklearn import linear_model
+from sklearn import svm
+from sklearn import neighbors
+from sklearn.lda import LDA
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
 
 begin_yr = 1751
 end_yr = 2008
@@ -35,8 +42,8 @@ def import_data():
 #Percent (ratio of sampling between Train and Test
 #Direction (boolean): True: Train data cut from ealier, False: Train data cut from later in history
 #Years: 
-def sample_cut(years, data, percent, direction):
-    targetVector = yearToTargetVector(years)    
+def sample_cut(years, data, percent, direction, shift_amt):
+    targetVector = yearToTargetVector(years, shift_amt)    
     tv = np.array([targetVector.tolist()])
     temp = np.concatenate((tv.T, data), axis = 1)
 
@@ -55,8 +62,8 @@ def sample_cut(years, data, percent, direction):
         test_target = targetVector[:cut]   
     return train, train_target, test, test_target
 
-def yearToTargetVector(years):
-    tmp = np.zeros(total_yrs)
+def yearToTargetVector(years, shift_amt):
+    tmp = np.zeros(total_yrs-shift_amt)
     for year in years:
         tmp[1+year-begin_yr] = 1
     return tmp
@@ -74,15 +81,16 @@ def sample_random(data, percent, direction):
     #TODO:
     return 0
 
-def train_model(train_matrix, target_vector):
+def train_model(train_matrix, target_vector, classifier):
     
     #Standardize features
     scaler = preprocessing.StandardScaler().fit(train_matrix)
     scaled_features = scaler.transform(train_matrix)
 
     #Fit logistic regression model
-    classifier = linear_model.LogisticRegression(penalty='l2',dual=False)
     classifier.fit(scaled_features,target_vector)
+    
+
 
     return classifier
 
@@ -93,21 +101,71 @@ def test_model(classifier, test_matrix, target_vector):
     scaler = preprocessing.StandardScaler().fit(test_matrix)
     scaled_features = scaler.transform(test_matrix)
 
+    TP = 0
+    FP = 0
+    TN = 0
+    FN = 0
+    
     predicted_eruptions = classifier.predict(scaled_features)
-    for i,eruption in enumerate(predicted_eruptions):
-        print eruption
-        if eruption == 1:
+    for i,guessed_eruption in enumerate(predicted_eruptions):
+        #print eruption
+        if guessed_eruption == 1:
             if target_vector[i] == 1:
-                print True
+                #print "\t"+str(True)+" "+str(classifier)[:15]
+                TP += 1
             else:
-                print False
+                FP += 1
+                #print False
+    if TP + FP == 0:
+        print "\tNONE\t"+str(classifier)[:10]        
+    else:    
+        print "\t"+ str(float(TP)/(TP+FP))+"\t"+str(classifier)[:10]
+            
 
-		
-years = get_volcano_years()
-print years
-data = import_data()
+def shift(amount, data, years):
+    new_data = data[amount:]
+    new_years = []
+    for year in years:
+        if year+amount <= end_yr:
+            new_years.append(year+amount)
+    return new_data, new_years
 
-#print data[words.index('volcanos'), 1756-begin_yr]
-train, train_target, test, test_target = sample_cut(years, data, 0.8, True)
-classifier = train_model(train, train_target)
+				
+
+def spray_n_pray(data, years):
+    for i in range(3):
+        print "Shifting by: "+str(i)
+        data, years = shift(i, master_data, master_years)
+    
+    
+        #print data[words.index('volcanos'), 1756-begin_yr]
+        percentage  = 0.3
+        while percentage <= 1.0:
+            print "    Percentage "+str(percentage)
+            train, train_target, test, test_target = sample_cut(years, data, percentage, True, i)
+            
+            classifiers = [linear_model.LogisticRegression(penalty='l2',dual=False), 
+            linear_model.SGDClassifier(),
+            svm.LinearSVC(),
+            svm.SVC(kernel = 'rbf'),
+            linear_model.Perceptron(penalty='l1'),
+            linear_model.Perceptron(penalty='l2',n_iter = 25),
+            neighbors.KNeighborsClassifier(),
+            LDA(),
+            tree.DecisionTreeClassifier(),
+            RandomForestClassifier(n_estimators=10)]
+                
+            for classifier_choice in classifiers:
+                #print classifier_choice
+                classifier = train_model(train, train_target, classifier_choice)
+                accuracy = test_model(classifier, test, test_target)
+            percentage += 0.05
+
+master_years = get_volcano_years()
+master_data = import_data()
+            
+data, years = shift(1, master_data, master_years)
+train, train_target, test, test_target = sample_cut(years, data, 0.55, True, 1)
+classifier_choice = RandomForestClassifier(n_estimators=10)
+classifier = train_model(train, train_target, classifier_choice)
 accuracy = test_model(classifier, test, test_target)
